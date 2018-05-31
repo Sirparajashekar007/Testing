8e5359c2506c82b8d2e45d87f4fc891df081f4c2
Rajashekar Sirpa <v-rsirp@microsoft.com>
2018-05-31 13:56:07 +0530

    Committing Practice Solution Changes

---
+++ ConsoleApplication2/ConsoleApplication2/ExceptionClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ConsoleApplication2
+{
+    [Serializable]
+    internal class ExceptionClass : Exception
+    {
+        private Exception innerException;
+
+        public ExceptionClass()
+        {
+        }
+
+        public ExceptionClass(Exception innerException)
+        {
+            this.innerException = innerException;
+        }
+
+        public ExceptionClass(string message) : base(message)
+        {
+            Console.WriteLine("Message:-" + message);
+            Console.ReadLine();
+        }
+
+        public ExceptionClass(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExceptionClass(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////int[] a = { 10, 5, 1, 15 };
+            ////Console.WriteLine("Please enter input");
+            ////int b = Convert.ToInt32(Console.ReadLine());
+            ////for (int i = 0; i < a[i] - 1; i++)
+            ////{
+            ////    if (a[i] == b)
+            ////    {
+            ////        Console.WriteLine("the given number {0} found out at the location {1}", a[i], i);
+            ////        Console.ReadLine();
+            ////    }
+            ////}
+            int a = 1, b = 0;
+            try
+            {
+                int c = a / b;
+            }
+            catch (Exception ex)
+            {
+                throw new ExceptionClass(ex.Message);
+            }
+        }
+    }
+}
